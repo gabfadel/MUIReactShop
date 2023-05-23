94cc642b35f8a28e9c5a7fb9a5438e9347962134
gabriel.faria-at-325485480401 <A/PpARTh6b6kxlSwx98GMe0TxlahiISbPK8z2URXZjY=>
2023-05-22 21:46:17 -0300

    product meta

--- materialui/src/components/product/productMeta.js
+++ materialui/src/components/product/productMeta.js
@@ -6,6 +6,7 @@ export default function ProductMeta ({product, matches}){
     return(
         <ProductMetaWrapper>
             <Typography variant={matches?'h6':'h5'} lineheight={2}>{product.name}</Typography>
+            <Typography variant={matches? 'caption' : 'body1'}>{product.price}</Typography>
         </ProductMetaWrapper>
     )
 }
