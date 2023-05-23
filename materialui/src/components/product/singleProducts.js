e9157d9d4b157680b06427f309bc0d3ad2b6c3a3
gabriel.faria-at-325485480401 <A/PpARTh6b6kxlSwx98GMe0TxlahiISbPK8z2URXZjY=>
2023-05-22 21:49:57 -0300

    product meta

--- materialui/src/components/product/singleProducts.js
+++ materialui/src/components/product/singleProducts.js
@@ -1,10 +1,12 @@
-import { Product, Productimage } from "../../styles/Products";
+import { Product, ProductMetaWrapper, Productimage } from "../../styles/Products";
+import ProductMeta from "./productMeta";
 
 
 export default function SingleProduct({product, matches}){
     return(
 <Product>
     <Productimage src={product.image} />
+    <ProductMeta product={product} matches={matches} />
 
     
 </Product>
